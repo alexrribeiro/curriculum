7bf3e3accd8a1653d60ab45dab2bb329a2e8a64a
alexrribeiro <alexramosribeiro@gmail.com>
2022-03-22 13:00:06 -0300

    Alterações JavaScript - 1

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-// const header = document.querySelector('.cabecalho');
-// const secoes = document.querySelectorAll('.secao');
-
-
-// function fixaCabecalho() {
-//     if (window.scrollY > 0) {
-//         header.classList.add('cabecalho-fixo');
-//     } else {
-//         header.classList.remove('cabecalho-fixo');
-//     }
-// }
-
-// window.addEventListener('scroll', () => {
-//     fixaCabecalho();
-// });
-
 const secaoInicio = document.querySelector('#inicio');
 
 function deslocaInicio() {
@@ -24,6 +8,44 @@ function deslocaInicio() {
     }
 }
 
+const botaoTopo = document.querySelector('.botao-topo');
+
+function voltarParaOTopo() {
+    if (window.scrollY >= 560) {
+        botaoTopo.classList.add('show');
+    }
+    else {
+        botaoTopo.classList.remove('show');
+    }
+}
+
+const sections = document.querySelectorAll('main section[id]');
+
+function ativarMenuSecaoAtual() {
+    const checkpoint = window.scrollY + (window.innerHeight / 8) * 4;
+    
+    for (const section of sections) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const sectionId = section.getAttribute('id');
+
+        const checkpointStart = checkpoint >= sectionTop;
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+        // if (checkpointStart && checkpointEnd) {
+        //     document
+        //         .querySelector('nav ul li a[href*=' + sectionId + ']')
+        //         .classList.add('active');
+        // } else {
+        //     document
+        //         .querySelector('nav ul li a[href*=' + sectionId + ']')
+        //         .classList.remove('active');
+        // }
+    }
+}
+
 window.addEventListener('scroll', () => {
     deslocaInicio();
+    voltarParaOTopo();
+    ativarMenuSecaoAtual();
 })
