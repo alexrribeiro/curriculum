e1fa2bb931cc77c878d82cca8bd34b974af98fa7
= <=>
2022-03-23 00:09:53 -0300

    JavaScript - 2, Inserção de Ícones,  Espaçamento

--- main.js
+++ main.js
@@ -32,15 +32,11 @@ function ativarMenuSecaoAtual() {
         const checkpointStart = checkpoint >= sectionTop;
         const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
 
-        // if (checkpointStart && checkpointEnd) {
-        //     document
-        //         .querySelector('nav ul li a[href*=' + sectionId + ']')
-        //         .classList.add('active');
-        // } else {
-        //     document
-        //         .querySelector('nav ul li a[href*=' + sectionId + ']')
-        //         .classList.remove('active');
-        // }
+        if (checkpointStart && checkpointEnd) {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('ativo');
+        } else {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('ativo');
+        }
     }
 }
 
